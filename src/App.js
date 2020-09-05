7d3f5fe18cc4f5ead66a44dc7484777ca0ff0b7e
Wicak <adityad.wck@gmail.com>
2020-09-05 14:49:49 +0700

    add state for follow button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
+
+const Profile = (props) => {
+  const [isFollowed,setIsFollowed] = useState(false)
+  return (
+    <React.Fragment>
+      <div className="App">
+        <img src="https://via.placeholder.com/300" alt="profile picture" />
+        <div>@{props.username}</div>
+        <button onClick={() => setIsFollowed(!isFollowed)}>{isFollowed? "Unfollow" : "Follow"}</button>
+        <div>Posts</div>
+        <div>Follower</div>
+        <div>Following</div>
+        <div>Bio</div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+const Post = (props) => (
+  <React.Fragment>
+    <div className="App">
+      <img src={props.src} alt="post" />
+    </div>
+  </React.Fragment>
+);
 
 function App() {
+  const username = "instagram";
   return (
-    <div className="App">
-      <img src="https://via.placeholder.com/300" alt="profile picture" />
-      <div>@instagram</div>
-      <button>Follow</button>
-      <div>Posts</div>
-      <div>Follower</div>
-      <div>Following</div>
-      <div>Bio</div>
+    <div>
+      <Profile username={username} />
+      <Post src="https://via.placeholder.com/150" />
     </div>
   );
 }

57bcb2762b3ac2059d07798f81a89ea1d03271b6
Wicak <adityad.wck@gmail.com>
2020-09-05 14:22:50 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="https://via.placeholder.com/300" alt="profile picture" />
+      <div>@instagram</div>
+      <button>Follow</button>
+      <div>Posts</div>
+      <div>Follower</div>
+      <div>Following</div>
+      <div>Bio</div>
     </div>
   );
 }
